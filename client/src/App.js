8d955a2420e13edde683a704a0cbc2299dd7e613
Hayat Tamboli <hayat.tamboli@gmail.com>
2021-04-05 10:28:56 +0530

    made the home page

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,30 @@
 import React from "react";
+import "./App.css";
+import Notes from "./Notes-pana.png";
 
-function App(){
-  return(
-    <div>
-      <h1>Hello world</h1>
+function App() {
+  return (
+    <div className="App">
+      <div className="header">
+        <h1>Curly Notes</h1>
+        <div>
+          <button className="secondary-button">Log in</button>
+          <button>Sign up</button>
+        </div>
+      </div>
+      <div className="home">
+        <div>
+          <h1 className="title">
+            Store notes where <br />
+            they belong
+          </h1>
+          <br />
+          <p className="subtitle">no more sticky notes here and there</p>
+          <br />
+          <button>Sign up</button>
+        </div>
+        <img src={Notes} alt="" width="400px" />
+      </div>
     </div>
   );
 }
